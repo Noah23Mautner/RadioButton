74f3831af3f15298ca127fa73888623821b5154b
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-02-11 17:36:11 +0100

    Add files via upload

---
+++ Radiobutton/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Radiobutton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                textBox.Text = "Rezervacija za Avion.";
+            }
+            else if (radioButton2.Checked)
+            {
+                textBox.Text = "Rezervacija za Brod.";
+            }
+            else if (radioButton3.Checked)
+            {
+                textBox.Text = "Rezervacija za Auto.";
+            }
+            else
+            {
+                MessageBox.Show("Molimo odaberite rezervaciju.", "Informacija", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
